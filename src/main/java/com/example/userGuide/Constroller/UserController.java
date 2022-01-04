565aef93f72a814a06f0b87c23b481976001c23f
Rahaman <mdshahidur_rahaman1@baylor.edu>
2022-01-04 01:27:22 -0600

    second commit

---
+++ src/main/java/com/example/userGuide/Constroller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.userGuide.Constroller;
+
+import com.example.userGuide.Service.UserService;
+import com.example.userGuide.model.User;
+import com.example.userGuide.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("user/")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("users")
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+}
---
+++ src/main/java/com/example/userGuide/Service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.userGuide.Service;
+
+import com.example.userGuide.model.User;
+import com.example.userGuide.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("users")
+    public List<User> getUsers() {
+        return this.userRepository.findAll();
+    }
+}
